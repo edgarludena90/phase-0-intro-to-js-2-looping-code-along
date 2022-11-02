f6578c95ee939492cf1d61360e63fd07f377d2c4
edgar ludena <edgar.ludena90@gmail.com>
2022-11-02 18:01:12 -0400

    commit tree c06aef31fabe0aa49c6039c76707e8f91eb6ffcc

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts){
+  for(let i = 0; i < gifts.length; i++){
+  console.log(`Wrapped ${gifts[i]}and add a bow! `)
+}
+  return gifts
+}
+console.log(gifts)
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+  }
+  return thankYouCards
+}
+function countDown( startingNumber ) {
+  while ( startingNumber > 0 ) {
+    console.log( startingNumber );
+    startingNumber -= 1;
+  }
+  console.log( startingNumber );
+}
